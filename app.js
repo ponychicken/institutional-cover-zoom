74352cc4c116e14d0b845ee207060d5a1b54c8be
Leo Koppelkamm <hello@leo-koppelkamm.de>
2015-01-14 16:10:30 +0100

    Update 2015-01-14T15:10:27.711Z

--- app.js
+++ app.js
@@ -21,3 +21,6 @@ if (typeof urlVars.subtitle !== 'undefined') {
 } else {
   var coverSubtitle = 'Subtitle was not passed';
 }
+
+document.querySelector('h1').innerText = coverTitle;
+document.querySelector('h2').innerText = coverSubtitle;
--- src/app.js
+++
@@ -1,2 +0,0 @@
-document.querySelector('h1').innerText = coverTitle;
-document.querySelector('h2').innerText = coverSubtitle;
